18d53c4be29b59546c56e13bc023f66dbc57c938
thomas <t.wirth@campus.tu-berlin.de>
2017-01-07 15:40:28 +0100

    Create simple, basic data models

---
+++ src/main/java/de/tu_berlin/dima/niteout/pt/data/Location.java
@@ -0,0 +1,25 @@
+package de.tu_berlin.dima.niteout.pt.data;
+
+/**
+ * The location we will use in the NiteOut-PublicTransport module
+ */
+public class Location {
+    private String latitude;
+    private String longitude;
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+}
