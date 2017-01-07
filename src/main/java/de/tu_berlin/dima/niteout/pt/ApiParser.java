3623234c1947e0cafa91d82cd9f6c092472f8ad5
thomas <t.wirth@campus.tu-berlin.de>
2017-01-07 12:30:30 +0100

    Add example api service usage

---
+++ src/main/java/de/tu_berlin/dima/niteout/pt/ApiParser.java
@@ -0,0 +1,15 @@
+package de.tu_berlin.dima.niteout.pt;
+
+import com.google.maps.*;
+
+/**
+ * Class which calls api and creates java objects from the call
+ */
+public class ApiParser {
+
+    public boolean runApiCall() {
+        GeoApiContext context = new GeoApiContext().setApiKey("AIza...");
+        return true;
+    }
+
+}
