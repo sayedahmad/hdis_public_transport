30d06a9dee85127b07c5c01a048a14ee7638392f
Andres Ardila <a.ardila@campus.tu-berlin.de>
2017-01-14 15:45:35 +0100

    Adding constructor to Location

--- src/main/java/de/tu_berlin/dima/niteout/routing/data/Location.java
+++ src/main/java/de/tu_berlin/dima/niteout/routing/data/Location.java
@@ -7,6 +7,11 @@ public class Location {
     private double latitude;
     private double longitude;
 
+    public Location(double latitude, double longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
     /**
      * @return the latitude of the location
      */
