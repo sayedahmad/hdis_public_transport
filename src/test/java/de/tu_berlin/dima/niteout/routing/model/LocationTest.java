975619229d53143e429a22a08b8dc58da37b6367
Amin Gul Zakhil <amingull@gmail.com>
2017-01-24 15:57:37 +0100

    Create new test for Location

--- src/test/java/de/tu_berlin/dima/niteout/routing/model/LocationTest.java
+++ src/test/java/de/tu_berlin/dima/niteout/routing/model/LocationTest.java
@@ -2,6 +2,9 @@ package de.tu_berlin.dima.niteout.routing.model;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
+
 /**
  * Test class for {@link Location}.
  */
@@ -9,6 +12,17 @@ public class LocationTest {
 
     @Test
     public void test() {
-        //TODO add tests
+    	 //TODO add tests
+    	//Given
+    	double latitude=13.32697;
+    	double longitude=52.51221;
+    	
+    			Location locTest=new Location(latitude, longitude);
+    			
+    			//Then
+    			//Assert.assertTrue(locTest.getLatitude().equals(latitude));
+    			//Assert.assertTrue(locTest.getLongitude().equals(longitude));
+    			assertEquals(locTest.getLatitude(),latitude, 0);
+    			assertEquals(locTest.getLongitude(),longitude, 0);
     }
 }
