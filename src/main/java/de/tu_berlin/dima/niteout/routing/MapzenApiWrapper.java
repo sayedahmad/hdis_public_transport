d608f970e5867ac8b4ec13f84eb3615f708425e5
Andres Ardila <a.ardila@campus.tu-berlin.de>
2017-01-26 14:07:32 +0100

    Reworked our public API & Mapzen wrappers - Added new methods to public API which take in a TransportMode parameter and removed the old ones - Gave a single interface to both Mapzen moility and matrix services under a single API

--- src/main/java/de/tu_berlin/dima/niteout/routing/MapzenApiWrapper.java
+++ src/main/java/de/tu_berlin/dima/niteout/routing/MapzenApiWrapper.java
@@ -1,100 +1,30 @@
 package de.tu_berlin.dima.niteout.routing;
 
-
 import de.tu_berlin.dima.niteout.routing.model.Location;
-import de.tu_berlin.dima.niteout.routing.model.mapzen.CostingModel;
+import de.tu_berlin.dima.niteout.routing.model.TimeMatrixEntry;
 
-import java.io.*;
-import java.net.*;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import javax.json.Json;
-import javax.json.JsonObject;
-import javax.json.JsonObjectBuilder;
-import javax.json.JsonReader;
+import java.io.IOException;
+import java.util.List;
 
-
-class MapzenApiWrapper {
+/**
+ * Created by aardila on 1/26/2017.
+ */
+public class MapzenApiWrapper {
 
     private final String apiKey;
-    private final String uriFormat = "https://valhalla.mapzen.com/route?json=%s&api_key=%s";
-
-    private static final DateTimeFormatter ISO8601_DATE_TIME_FORMATTER =
-            DateTimeFormatter.ofPattern("YYYY-MM-DD'T'HH:mm");
-
 
     public MapzenApiWrapper(String apiKey) {
-
-        if (apiKey == null || apiKey.trim().length() == 0)
-            throw new IllegalArgumentException("apiKey cannot be null or empty");
-
         this.apiKey = apiKey;
     }
 
-    private JsonObject getRouteResponse(Location start, Location destination, CostingModel costingModel) throws MalformedURLException, IOException {
-        return getRouteResponse(start, destination, costingModel, null);
+    public int getWalkingTripTime(Location start, Location destination) throws IOException {
+        MapzenMobilityApiWrapper mobilityWrapper = new MapzenMobilityApiWrapper(apiKey);
+        return mobilityWrapper.getWalkingTripTime(start, destination);
     }
 
-    public int getWalkingTripTime(Location start, Location destination) throws Exception {
-        int tripDuration = -1;
+    public List<TimeMatrixEntry> getWalkingMatrix(Location[] startLocations, Location[] destinationLocations) throws IOException {
 
-        try {
-            JsonObject response = getRouteResponse(start, destination, CostingModel.PEDESTRIAN);
-            tripDuration = response.getJsonObject("trip").getJsonObject("summary").getInt("time");
-        } catch (MalformedURLException e) {
-            e.printStackTrace(); //TODO
-        } catch (IOException e) {
-            e.printStackTrace(); //TODO
-        }
-
-        return tripDuration;
-    }
-
-    public int getPublicTransportTripTime(Location start, Location destination, LocalDateTime departureTime) {
-        int tripDuration = -1;
-
-        try {
-            //WARNING: multimodal currently supports pedestrian and transit. In the future, multimodal will return a combination of all modes of transport (including auto).
-            JsonObject response = getRouteResponse(start, destination, CostingModel.MULTIMODAL, departureTime);
-            tripDuration = response.getJsonObject("trip").getJsonObject("summary").getInt("time");
-        } catch (MalformedURLException e) {
-            e.printStackTrace(); //TODO
-        } catch (IOException e) {
-            e.printStackTrace(); //TODO
-        }
-
-        return tripDuration;
-    }
-
-    private JsonObject getRouteResponse(
-            Location start, Location destination,
-            CostingModel costingModel, LocalDateTime departureTime) throws MalformedURLException, IOException {
-
-        JsonObjectBuilder jsonObjectBuilder = Json.createObjectBuilder()
-                .add("locations", Json.createArrayBuilder()
-                        .add(Json.createObjectBuilder()
-                                .add("lat", start.getLatitude())
-                                .add("lon", start.getLongitude()))
-                        .add(Json.createObjectBuilder()
-                                .add("lat", destination.getLatitude())
-                                .add("lon", destination.getLongitude())))
-                .add("costing", costingModel.getApiString());
-
-        if (departureTime != null) {
-
-            jsonObjectBuilder.add("date_time", Json.createObjectBuilder()
-                    .add("type", 1)
-                    .add("value", departureTime.format(ISO8601_DATE_TIME_FORMATTER)));
-        }
-
-        JsonObject json = jsonObjectBuilder.build();
-
-        String uri = String.format(this.uriFormat, json, this.apiKey);
-        URL url = new URL(uri);
-        InputStream is = url.openStream();
-        JsonReader jsonReader = Json.createReader(is);
-        JsonObject jsonObject = jsonReader.readObject();
-
-        return jsonObject;
+        MapzenMatrixApiWrapper matrixWrapper = new MapzenMatrixApiWrapper(this.apiKey);
+        return matrixWrapper.getWalkingMatrix(startLocations, destinationLocations);
     }
 }
